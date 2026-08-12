64920e98237049d8ceec5774ac0be84abd06132f
Arseniy Kushnir <arseniykushnir2007@gmail.com>
2026-08-12 13:06:50 +0300

    Fixed a JS script

--- main.py
+++ main.py
@@ -36,19 +36,12 @@ manager = ConnectionManager()
 @app.get("/")
 async def index(request: Request):
     client_id = str(uuid.uuid4())
-    is_https = request.url.scheme == "https"
-    if is_https:
-        ws_protocol = "wss"
-        host = request.url.hostname
-    else:
-        ws_protocol = "ws"
-        host = request.url.netloc
 
     return templates.TemplateResponse(
         request,
         "index.html",
         {
-            "ws_url": f"{ws_protocol}://{host}/ws/{client_id}"
+            "client_id": client_id
         }
     )
 
